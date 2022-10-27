74908544f6d03d7d4c6a396fa5f22b0ced49e137
Dan Adwindea <danadwnd@gmail.com>
2022-10-27 16:34:09 +0700

    set logger middleware, set cors

---
+++ config/allowedOrigins.js
@@ -0,0 +1,8 @@
+const allowedOrigins = [
+    'http://collad.adwindea.my.id',
+    'https://collad.adwindea.my.id',
+    'http://www.collad.adwindea.my.id',
+    'https://www.collad.adwindea.my.id',
+]
+
+module.exports = allowedOrigins
